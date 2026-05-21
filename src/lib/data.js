b78c5a1a7c70709a2dab9fe81c064142a33c707a
rupesh-saha <saharupesh291@gmail.com>
2026-05-21 21:13:46 +0600

    bug fixes(2)

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,15 +1,15 @@
 export const getFeaturedCard = async() => {
-  const res = await fetch('http://localhost:3000/featured.json', {cache: 'no-cache'});
+  const res = await fetch('https://a08-terrann.vercel.app/featured.json', {cache: 'no-cache'});
   return res.json();
 }
 
 export const getAllCards = async() => {
-  const res = await fetch('http://localhost:3000/all-tiles.json', {cache: 'no-cache'});
+  const res = await fetch('https://a08-terrann.vercel.app/all-tiles.json', {cache: 'no-cache'});
   return res.json();
 }
 
 export const tileDetail = async(id) => {
-  const res = await fetch('http://localhost:3000/all-tiles.json', {cache: 'no-cache'});
+  const res = await fetch('https://a08-terrann.vercel.app/all-tiles.json', {cache: 'no-cache'});
   
   const allTiles = await res.json();
 
