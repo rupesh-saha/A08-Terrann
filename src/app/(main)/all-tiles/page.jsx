e395ac490296d33022fa15afafedf19596fc351c
rupesh-saha <saharupesh291@gmail.com>
2026-05-21 14:18:16 +0600

    search bar implemented

--- src/app/(main)/all-tiles/page.jsx
+++ src/app/(main)/all-tiles/page.jsx
@@ -1,9 +1,36 @@
+'use client'
 import TileCard from "@/Components/TileCard";
 import { getAllCards } from "@/lib/data";
+import React, { useState, useEffect } from 'react';
 
-const AllTiles = async () => {
-  const Tiles = await getAllCards();
-  console.log(Tiles);
+const AllTiles = () => {
+
+  const [allTiles, setAllTiles] = useState([]);
+  const [displayedTiles, setDisplayedTiles] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    const loadTiles = async () => {
+      const res = await fetch('http://localhost:3000/all-tiles.json');
+      const data = await res.json();
+
+      setAllTiles(data);
+      setDisplayedTiles(data);
+    };
+    loadTiles();
+  }, []);
+
+  const handleSearchClick = () => {
+    const query = searchInput.toLowerCase().trim();
+
+    const filtered = allTiles.filter(tile =>
+      tile.title.toLowerCase().includes(query)
+    );
+
+    setDisplayedTiles(filtered);
+  };
+
+
 
   return (
     <div className="min-h-screen bg-[#fcfcfc]">
@@ -19,12 +46,20 @@ const AllTiles = async () => {
             Explore our complete collection of premium ceramic, porcelain, and natural stone tiles. Find the perfect foundation for your next architectural project.
           </p>
 
+          <div className="max-w-2xl mx-auto flex gap-2">
+            <input type="text" placeholder="Search collections..." value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className="input input-bordered w-full bg-white border-gray-200 focus:border-gray-900 focus:outline-none rounded-none text-gray-900 transition-colors" />
+
+            <button onClick={handleSearchClick} className="btn bg-[#141311] text-white hover:bg-gray-800 border-none rounded-none px-8 font-bold uppercase  text-xs transition-all duration-300">
+              Search
+            </button>
+          </div>
+
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-16">
 
           {
-            Tiles.map(tile => <TileCard key={tile.id} tile={tile}></TileCard>)
+            displayedTiles.map(tile => <TileCard key={tile.id} tile={tile}></TileCard>)
           }
 
         </div>
